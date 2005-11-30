df180fe7b7120a8bcc782a139098d9c0e482ccae
Yves Junqueira <yves.junqueira@gmail.com>
2005-11-30 20:30:54 +0000

    * Melhor descrição dos pacotes sysinfo e comm

--- comm/__init__.py
+++ comm/__init__.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Provides methods for communication with the CACIC server.
 
+Currently it uses HTTP which is the only method supported by
+the server.
+"""
--- sysinfo/__init__.py
+++ sysinfo/__init__.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+"""Provides methods for collecting system information in
+a portable way, for both Linux and Windows.
+"""
