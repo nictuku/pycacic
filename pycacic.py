6d8b13c44cf2c10e12239e1b1506c2e95bd3e04b
Yves Junqueira <yves.junqueira@gmail.com>
2006-01-10 20:20:44 +0000

    * Studying a decent logger solution

--- pycacic.py
+++ pycacic.py
@@ -30,16 +30,38 @@ metodologias e estruturas.
 import agent
 import agent.config
 
+import logging
+
+logger = logging.getLogger("cacic")
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - \
+%(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.debug("debug message")
+logger.info("info message")
+logger.warn("warn message")
+logger.error("error message")
+logger.critical("critical message")
+
+logger.debug("Loading agent.http")
+
 helloCACIC = agent.http
+
+logger.debug("Loading agent.config.load()")
 cacic = agent.config.load()
 info = cacic.info
 #print "hw:", hw.__dict__
 # get_config cria a maquina, set_tcp_ip manda infos
 
+logger.info("Getting configuration from the server")
 helloCACIC.post_info(info,'cacic','cacic2/ws/get_config.php')
+logger.info("Posting network information")
 helloCACIC.post_info(info,'cacic','cacic2/ws/set_tcp_ip.php')
+logger.info("Posting hardware information")
 helloCACIC.post_info(info,'cacic','cacic2/ws/set_hardware.php')
 
-
-print "weee, foi"
+logger.info("Finished execution sucessfuly")
 
