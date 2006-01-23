6f6f9b02125a959bb836ef2d22f2d385f9ee2641
Yves Junqueira <yves.junqueira@gmail.com>
2006-01-23 20:52:35 +0000

    * apt.cache.installed_packages implemented

--- sysinfo/linux/apt.py
+++ sysinfo/linux/apt.py
@@ -30,38 +30,52 @@ import sys
 
 class cache:
 
-    # FIXME: change to InitSystem, InitConfig and hide info messages
-    apt_pkg.init()
-    # get caches
-    cache = apt_pkg.GetCache()
-    #depcache = apt_pkg.GetDepCache(cache)
+    # ['Name' : 'Version']
+    installed_packages = {}
+    
+    def __init__(self):
 
-    # read the pin files
-    #depcache.ReadPinFile()
-    # read the synaptic pins too
-    #if os.path.exists(SYNAPTIC_PINFILE):
-    #    depcache.ReadPinFile(SYNAPTIC_PINFILE)
-    
-    # init the depcache
-    #depcache.Init()
-    
-    #if depcache.BrokenCount > 0:
-    #    sys.stderr.write("E: BrokenCount > 0")
-    #    sys.exit(-1)
-    
-    #depcache.Upgrade()
-    
-    # version comparison function:
-    # http://mail.python.org/pipermail/python-list/2005-March/272909.html
-    for pkg in cache.Packages:
-        if pkg.CurrentVer:
-            print pkg.Name
-        #    print "ver", pkg.CurrentVer.VerStr
-            
-            #for depend in pkg.CurrentVer.DependsList.get("Depends", []):
-            #    print "depends", depend,
-     #   if depcache.MarkedInstall(pkg) or depcache.MarkedUpgrade(pkg):
-     #       if depcache.GetCandidateVer(pkg) != pkg.CurrentVer:
-     #           print "Update", pkg.Name
-        #    sys.exit(0)
-            
+        self.installed_packages = self.get_installed_packages()
+
+    def get_installed_packages(self):
+        inst_pkgs = {}
+        
+        # FIXME: change to InitSystem, InitConfig and hide info messages
+        apt_pkg.init()
+        # get caches
+        cache = apt_pkg.GetCache()
+        #depcache = apt_pkg.GetDepCache(cache)
+
+        # read the pin files
+        #depcache.ReadPinFile()
+        # read the synaptic pins too
+        #if os.path.exists(SYNAPTIC_PINFILE):
+        #    depcache.ReadPinFile(SYNAPTIC_PINFILE)
+        
+        # init the depcache
+        #depcache.Init()
+        
+        #if depcache.BrokenCount > 0:
+        #    sys.stderr.write("E: BrokenCount > 0")
+        #    sys.exit(-1)
+        
+        #depcache.Upgrade()
+        
+        # version comparison function:
+        # http://mail.python.org/pipermail/python-list/2005-March/272909.html
+        for pkg in cache.Packages:
+            if pkg.CurrentVer:
+                inst_pkgs[pkg.Name] = pkg.CurrentVer
+            #    print "ver", pkg.CurrentVer.VerStr
+                
+                #for depend in pkg.CurrentVer.DependsList.get("Depends", []):
+                #    print "depends", depend,
+         #   if depcache.MarkedInstall(pkg) or depcache.MarkedUpgrade(pkg):
+         #       if depcache.GetCandidateVer(pkg) != pkg.CurrentVer:
+         #           print "Update", pkg.Name
+            #    sys.exit(0)
+        return inst_pkgs        
+
+if __name__ == '__main__':
+    c = cache()
+    print "oi", c.installed_packages
